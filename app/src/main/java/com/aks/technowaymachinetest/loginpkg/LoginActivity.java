b575af0a9898087c5aac3252ab90ac8664b8c804
ajayksivadasan <ajaykundukade@gmail.com>
2021-06-19 18:22:07 +0530

    initial commit

---
+++ app/src/main/java/com/aks/technowaymachinetest/loginpkg/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.aks.technowaymachinetest.loginpkg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aks.technowaymachinetest.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
